4f15e1777f026a23304a5f9081950d3af76e5bf4
Matthew Everette <mattheweverette@gmail.com>
2021-10-15 20:19:03 -0400

    Initial Commit

---
+++ MinecraftClone/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MinecraftClone
+//
+//  Created by Matthew Everette on 10/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
